3d85cb1e04a84b287e94233af36ee09f0d03dcdb
NazarMavrov <152779009+NazarMavrov@users.noreply.github.com>
2024-04-21 23:20:42 +0300

    hw8-05-js

--- js/gallery.js
+++ js/gallery.js
@@ -78,7 +78,7 @@ const murkup = images.reduce((html, image) => {
     </a>
     </li>
     `);
-}, "");
+},"");
 const imageList = document.querySelector('.gallery');
 
 imageList.insertAdjacentHTML('beforeend', murkup);
